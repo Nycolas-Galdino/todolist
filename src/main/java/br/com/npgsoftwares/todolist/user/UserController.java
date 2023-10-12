326702fc5b7efe2771ec946df814f81beb18fa84
Nycolas Galdino <nycolaspimentel12@gmail.com>
2023-10-12 18:13:40 -0300

    modified: Config the temp database

--- src/main/java/br/com/npgsoftwares/todolist/user/UserController.java
+++ src/main/java/br/com/npgsoftwares/todolist/user/UserController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
     
+    
+
     @PostMapping("/")
     public void create(@RequestBody UserModel userModel){
         System.out.println(userModel.getName());
