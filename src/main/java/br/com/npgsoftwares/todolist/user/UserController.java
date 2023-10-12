edb6c4509db22ce49242b5a3ab3b0bad839fee74
Nycolas Galdino <nycolaspimentel12@gmail.com>
2023-10-12 18:21:20 -0300

    modified: Add fnct to add User in Database

--- src/main/java/br/com/npgsoftwares/todolist/user/UserController.java
+++ src/main/java/br/com/npgsoftwares/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.npgsoftwares.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
     
-    
+    @Autowired
+    private IUserRepository userRepository;
 
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
+    public UserModel create(@RequestBody UserModel userModel){
         System.out.println(userModel.getName());
         System.out.println(userModel.getUsername());
         System.out.println(userModel.getPassword());
+         
+        return this.userRepository.save(userModel);
     }
 }
