b765d1198b80e4e172e91a5798ed105ec72b6d06
Nycolas Galdino <nycolaspimentel12@gmail.com>
2023-10-13 13:47:51 -0300

    modified: Cripted pass in insert db

--- src/main/java/br/com/npgsoftwares/todolist/user/UserController.java
+++ src/main/java/br/com/npgsoftwares/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -27,6 +29,10 @@ public class UserController {
             System.out.print("O usuário já está cadastrado no sistema");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já está cadastrado no sistema");
         }
+
+        var passHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passHashred);
          
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
