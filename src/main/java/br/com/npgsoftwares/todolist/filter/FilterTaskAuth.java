b70b6c8810bf214a3a117dc3fd4b4bd851590589
Nycolas Galdino <nycolaspimentel12@gmail.com>
2023-10-13 18:17:29 -0300

    modified: Add relation in Task Create and User

--- src/main/java/br/com/npgsoftwares/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/npgsoftwares/todolist/filter/FilterTaskAuth.java
@@ -52,6 +52,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
                         // Verifica se a senha existe
                         var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
                         if(passwordVerify.verified){
+                            request.setAttribute("idUser", user.getId());
                             filterChain.doFilter(request, response);
                         } else {
                             response.sendError(401, "A senha está incorreta");
