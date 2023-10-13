97cb5336d71302dec6abde057201f9bf6587574e
Nycolas Galdino <nycolaspimentel12@gmail.com>
2023-10-13 18:01:38 -0300

    feat: Add the Task Manager in Project

---
+++ src/main/java/br/com/npgsoftwares/todolist/task/TaskModel.java
@@ -0,0 +1,33 @@
+package br.com.npgsoftwares.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String description;
+
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+    
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    private UUID idUser;
+}
