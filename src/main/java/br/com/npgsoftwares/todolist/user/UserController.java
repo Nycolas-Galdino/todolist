b0de51474f85c24fe93c7165e599a8953fa1cb74
Nycolas Galdino <nycolaspimentel12@gmail.com>
2023-10-13 09:33:06 -0300

    modified: Add single user verification

--- src/main/java/br/com/npgsoftwares/todolist/user/UserController.java
+++ src/main/java/br/com/npgsoftwares/todolist/user/UserController.java
@@ -1,6 +1,8 @@
 package br.com.npgsoftwares.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,11 +16,19 @@ public class UserController {
     private IUserRepository userRepository;
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel){
+    public ResponseEntity create(@RequestBody UserModel userModel){
         System.out.println(userModel.getName());
         System.out.println(userModel.getUsername());
         System.out.println(userModel.getPassword());
+
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null){
+            System.out.print("O usuário já está cadastrado no sistema");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já está cadastrado no sistema");
+        }
          
-        return this.userRepository.save(userModel);
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
