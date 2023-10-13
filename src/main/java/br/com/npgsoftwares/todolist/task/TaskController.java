d95620102099797983ac0877f12194ea983de348
Nycolas Galdino <nycolaspimentel12@gmail.com>
2023-10-13 18:38:48 -0300

    modified: Add date data validation

--- src/main/java/br/com/npgsoftwares/todolist/task/TaskController.java
+++ src/main/java/br/com/npgsoftwares/todolist/task/TaskController.java
@@ -1,8 +1,10 @@
 package br.com.npgsoftwares.todolist.task;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +20,19 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
-        System.out.println("Chegou no controller");
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         var idUser = request.getAttribute("idUser");
         taskModel.setIdUser((UUID) idUser);
-        return this.taskRepository.save(taskModel);
+
+        var currentDate = LocalDateTime.now();
+        if(currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())){
+            return ResponseEntity.status(400).body("A data de início / data de término deve ser maior do que a data atual");
+        } 
+        
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())){
+            return ResponseEntity.status(400).body("A data de término deve ser maior que a data de término");
+        } else {
+            return ResponseEntity.status(200).body(this.taskRepository.save(taskModel));
+        }
     }
 }
