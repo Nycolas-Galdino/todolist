2d48c2a92519b293e739fadbfa42cd99ab7099fd
Nycolas Galdino <nycolaspimentel12@gmail.com>
2023-10-14 16:38:40 -0300

    modified: Added task update logic

--- src/main/java/br/com/npgsoftwares/todolist/task/TaskController.java
+++ src/main/java/br/com/npgsoftwares/todolist/task/TaskController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.npgsoftwares.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -50,6 +51,11 @@ public class TaskController {
     @PutMapping("/{id}")
     public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id){
         var idUser = request.getAttribute("idUser");
+
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        Utils.copyNonNullProperties(taskModel, task);
+
         taskModel.setIdUser((UUID) idUser);
         taskModel.setId(id);
         return this.taskRepository.save(taskModel);
